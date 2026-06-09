e93bd557eee946fcc1348ac5a2cc92b4279d63ca
Ilya Shelkunov <shelkunow48@gmail.com>
2026-06-09 16:10:05 +0300

    feat: implement functional physics simulation with defensive collision and deltaTime scaling

--- src/components/CanvasContainer.tsx
+++ src/components/CanvasContainer.tsx
@@ -1,21 +1,44 @@
 "use client";
 
+import {
+  drawParticle,
+  generateParticles,
+  updateParticlePhisics,
+} from "@/lib/engine/particlesUtils";
+import { IParticle } from "@/lib/engine/types";
 import { useEffect, useRef } from "react";
 
 export default function CanvasContainer() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
+  const particlesRef = useRef<IParticle[]>([]);
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
-    ctx.fillStyle = "#0f172a";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#6366f1";
-    ctx.beginPath();
-    ctx.arc(canvas.width / 2, canvas.height / 2, 50, 0, Math.PI * 2);
-    ctx.fill();
+    const width = canvas.width;
+    const height = canvas.height;
+    particlesRef.current = generateParticles(500, width, height);
+    let animationId: number;
+    let lastTime: number = 0;
+    const loop = (time: number) => {
+      let dt = lastTime ? (time - lastTime) / 1000 : 0;
+      lastTime = time;
+      if (dt > 0.1) dt = 0;
+      ctx.fillStyle = "rgba(15, 23, 42, 0.25)";
+      ctx.fillRect(0, 0, width, height);
+      particlesRef.current.forEach((particle) => {
+        updateParticlePhisics(particle, width, height, dt);
+      });
+      particlesRef.current.forEach((particle) => {
+        drawParticle(ctx, particle);
+      });
+      animationId = requestAnimationFrame(loop);
+    };
+    animationId = requestAnimationFrame(loop);
+    return () => {
+      cancelAnimationFrame(animationId);
+    };
   }, []);
 
   return (
